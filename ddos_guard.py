6688d875ac66a8d83a7c4178f81ab0a34f7f5e9e
e27182 <>
2022-03-05 05:03:46 -0500

    Simplification of ddosguard protection bypass

--- ddos_guard.py
+++ ddos_guard.py
@@ -11,25 +11,28 @@ from http.cookies import SimpleCookie
 from aiohttp import ClientSession
 
 
-async def bypass(url: str, cookies=None, *, session: ClientSession):
-    if cookies is None:
-        headers_1 = {
-            "Accept": "text/html",
-            "Accept-Language": "en-US",
-            "Connection": "keep-alive",
-            "Sec-Fetch-Dest": "document",
-            "Sec-Fetch-Mode": "navigate",
-            "Sec-Fetch-Site": "none",
-            "Sec-Fetch-User": "?1",
-            "TE": "trailers",
-            "DNT": "1",
-        }
-        async with session.get(url, headers=headers_1) as response_1:
-            if response_1.status not in (HTTPStatus.OK, HTTPStatus.FORBIDDEN):
-                return
-            cookies = response_1.cookies
+async def bypass(url: str, cookies=None, *, session: ClientSession, ignore_response: bool):
+    logging.debug('[ddos_guard.bypass] Started for %s', url)
 
-    logging.info("[ddos_guard.bypass] Parsed cookies from %r: %r", url, cookies or None)
+    headers_1 = {
+        "Accept": "text/html",
+        "Accept-Language": "en-US",
+        "Connection": "keep-alive",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "TE": "trailers",
+        "DNT": "1",
+    }
+
+    # sometimes DDosGuard returns only part of the cookies - bug? So, need to ask for several times for id to be there.
+    iter = 0
+    while (cookies is None or cookies.get("__ddgid") is None) and 5 > ++iter:
+        async with session.get(url, headers=headers_1) as response_1:
+            cookies.update(response_1.cookies)
+
+    logging.debug("[ddos_guard.bypass] Parsed cookies from %r: %r", url, cookies or None)
 
     headers_2 = {
         "Accept": "*/*",
@@ -40,34 +43,28 @@ async def bypass(url: str, cookies=None, *, session: ClientSession):
         "Sec-Fetch-Mode": "no-cors",
         "Sec-Fetch-Site": "cross-site"
     }
-    async with session.get("https://check.ddos-guard.net/check.js", cookies=cookies, headers=headers_2) as response_2:
-        text = await response_2.text()
+    async with session.get("https://check.ddos-guard.net/check.js", cookies=None, headers=headers_2) as response_2:
+        cookies_2 = response_2.cookies
 
-    match = re.search(r"'(?P<path>/.well-known/ddos-guard/id/\S+)'", text)
-    if not match:
-        return
+    logging.debug("[ddos_guard.bypass] Parsed cookies from check.js: %r", cookies_2)
 
-    path = match.groupdict()["path"]
-    logging.info("[ddos_guard.bypass] Retrieved path from ddos-guard's check.js: %r", path)
-    url_split = list(urllib.parse.urlsplit(url))
-    url_split[2] = path
-    ddos_guard_url = urllib.parse.urlunsplit(url_split)
+    domain = urllib.parse.urlparse(url).netloc
+    for c in cookies_2:
+        cookies_2[c]["domain"] = domain
 
-    headers_3 = {
-        "Accept": "image/webp,*/*",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate",
-        "Cache-Control": "no-cache",
-        "Referer": url,
-        "Sec-Fetch-Dest": "script",
-        "Sec-Fetch-Mode": "no-cors",
-        "Sec-Fetch-Site": "cross-site"
-    }
+    ddos_guard_cookies = SimpleCookie()
+    ddos_guard_cookies.update(cookies)
+    ddos_guard_cookies.update(cookies_2)
 
-    async with session.get(ddos_guard_url, cookies=cookies, headers=headers_3) as response_3:
-        ddos_guard_cookies = SimpleCookie()
-        ddos_guard_cookies.update(cookies)
-        ddos_guard_cookies.update(response_3.cookies)
+    logging.debug("[ddos_guard.bypass] Retrieved final cookies for %r: %r", url, ddos_guard_cookies)
 
-    logging.info("[ddos_guard.bypass] Retrieved final cookies from %r: %r", ddos_guard_url, ddos_guard_cookies)
-    return ddos_guard_cookies
+    async with session.get(url, cookies=ddos_guard_cookies, headers=headers_1) as response_12:
+            if response_12.status == HTTPStatus.OK:
+                logging.info("[ddos_guard.bypass] Protection was bypassed for %r: %r", url, ddos_guard_cookies)
+                if not ignore_response:
+                    data = await response_12.text()
+            if response_12.status != HTTPStatus.OK:
+                #data = await response_12.text()
+                logging.warning("[ddos_guard.bypass] Protection was NOT bypassed for %r: %r", url, ddos_guard_cookies)
+
+    return ddos_guard_cookies, response_12
